be802e7d47e6e088a3b4118a0bfb37126212a07e
Valentin Fernandez <fernandezpvalentin22@gmail.com>
2026-03-01 14:38:33 -0300

    logic1-

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,17 +1,59 @@
 import { Router } from "express";
-import ProductManager from "../managers/ProductManager.js";
+import Product from "../models/Product.model.js";
+import Cart from "../models/Cart.model.js";
 
 const router = Router();
-const manager = new ProductManager("./data/products.json");
 
-router.get("/", async (req, res) => {
-  const products = await manager.getProducts();
-  res.render("home", { products });
+
+router.get("/products", async (req, res) => {
+  try {
+    const { page = 1 } = req.query;
+
+    const result = await Product.paginate(
+      {},
+      {
+        page: parseInt(page),
+        limit: 5,
+        lean: true   
+      }
+    );
+
+    res.render("products", {
+      products: result.docs,
+      hasPrevPage: result.hasPrevPage,
+      hasNextPage: result.hasNextPage,
+      prevPage: result.prevPage,
+      nextPage: result.nextPage
+    });
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-router.get("/realtimeproducts", async (req, res) => {
-  const products = await manager.getProducts();
-  res.render("realTimeProducts", { products });
+
+router.get("/products/:pid", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.pid);
+    res.render("productDetail", { product });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+router.get("/carts/:cid", async (req, res) => {
+  try {
+    const cart = await Cart
+    .findById(req.params.cid)
+    .populate("products.product")
+    .lean();
+
+    res.render("cart", { cart });
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 export default router;
